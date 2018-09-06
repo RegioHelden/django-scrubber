edebdd56bff3462569dafb119a5a184c46272020
Leo Antunes <leo@costela.net>
2018-09-06 12:40:07 +0200

    FieldFunc: add connection_setup; add MD5 on sqlite support

--- runtests.py
+++ runtests.py
@@ -15,6 +15,7 @@ def run_tests(*test_args):
         test_args = ['tests']
 
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+    settings.INSTALLED_APPS.append('tests')
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
