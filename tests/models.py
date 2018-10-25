1af7a0e33e8782974a1e4e14049f38cf426e35f6
Benedikt Bauer <benedikt.bauer@ambient-innovation.com>
2018-10-25 15:37:08 +0200

    Add a test for the parameters and add an example in Readme

--- tests/models.py
+++ tests/models.py
@@ -7,6 +7,7 @@ class DataToBeScrubbed(models.Model):
     first_name = models.CharField(max_length=8)
     last_name = models.CharField(max_length=255)
     description = models.TextField()
+    date_past = models.DateField(blank=True, null=True)
 
 
 class DataFactory(DjangoModelFactory):
