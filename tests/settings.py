6346dd767e86685be38bdbfce1d250c0e7645e62
Benedikt Bauer <benedikt.bauer@ambient-innovation.com>
2019-11-08 16:52:25 +0100

    Fix unneccessary import and leftovers from file-based sqlite db

--- tests/settings.py
+++ tests/settings.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals, absolute_import
 
-import os
-
 import django
 
 DEBUG = True
 USE_TZ = True
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "uzbLoOIYlJnzGDYlUfynNyocjZH9NLSc3AAREwLDaugQkCzsQn"
@@ -28,7 +23,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sites",
     "django_scrubber",
-    "tests",
 ]
 
 SITE_ID = 1
