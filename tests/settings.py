fe580874e57a1269b36190fc83dbdeabdd27473f
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2019-11-08 16:06:13 +0100

    PR fixes

--- tests/settings.py
+++ tests/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = "uzbLoOIYlJnzGDYlUfynNyocjZH9NLSc3AAREwLDaugQkCzsQn"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
+        'NAME': ':memory:',
     }
 }
 
