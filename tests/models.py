6c42bacd0ad890804e74ae509089d510cb7f1156
Leo Antunes <leo@costela.net>
2019-12-02 22:33:07 +0100

    add empty and null scrubbers

--- tests/models.py
+++ tests/models.py
@@ -5,7 +5,7 @@ from factory.django import DjangoModelFactory
 
 class DataToBeScrubbed(models.Model):
     first_name = models.CharField(max_length=8)
-    last_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, null=True)
     description = models.TextField()
     ean8 = models.CharField(max_length=13)
     date_past = models.DateField(null=True)
