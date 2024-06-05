8ff16be589875262e10f32dd53b8c49cc6cb8b4a
Jens Nistler <opensource@jensnistler.de>
2024-06-05 09:21:51 +0200

    Bump version: 1.2.2 → 1.3.0

--- django_scrubber/__init__.py
+++ django_scrubber/__init__.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 
-__version__ = '1.2.2'
+__version__ = '1.3.0'
 
 defaults = {
     'SCRUBBER_RANDOM_SEED': 42,  # we prefer idempotent scrubbing
