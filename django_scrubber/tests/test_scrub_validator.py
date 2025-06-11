be3121707e17490602ebdd8748dc16fd881f7aa8
Jens Nistler <jens.nistler@regiohelden.de>
2025-06-11 08:18:23 +0000

    feat: fix command output

--- django_scrubber/tests/test_scrub_validator.py
+++ django_scrubber/tests/test_scrub_validator.py
@@ -1,3 +1,4 @@
+from io import StringIO
 from unittest import mock
 
 from django.core.management import call_command
@@ -9,10 +10,10 @@ from django_scrubber.services.validator import ScrubberValidatorService
 class TestScrubValidator(TestCase):
     def test_scrub_validator_regular(self):
         with self.assertRaises(SystemExit):
-            call_command("scrub_validation", verbosity=3)
+            call_command("scrub_validation", verbosity=3, stdout=StringIO())
 
     @mock.patch.object(ScrubberValidatorService, "process")
     def test_scrub_validator_service_called(self, mocked_method):
-        call_command("scrub_validation", verbosity=3)
+        call_command("scrub_validation", verbosity=3, stdout=StringIO())
 
         mocked_method.assert_called_once()
